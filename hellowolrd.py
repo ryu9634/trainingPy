34af55a925a4d46f73a68b1cff5fc1bc84a52b3a
ryu <ryusp0515@naver.com>
2024-09-21 21:05:06 +0900

    commit test(24.09.21)

---
+++ hellowolrd.py
@@ -0,0 +1,7 @@
+#python -m ildelib.ilde
+
+'''
+위의 명령어로 파이썬 아이들 생성가능
+'''
+
+print('hello wolrd')
---
+++ if절.py
@@ -0,0 +1,21 @@
+# if 문 사용방법 
+
+'''
+if ~ else
+
+if ~ elif
+
+if ~ elif ~ else
+
+'''
+
+#if 조건 : 
+#   실행코드
+
+'''
+조건이 참일때 실행됨 
+'''
+num = int(input('숫자 입력'))
+
+if num > 0 :
+    print('{}는 양수입니다.'.format(num))
